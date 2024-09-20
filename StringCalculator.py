cef5b213dd51fb5cf219c48ce2d24153889769c3
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-09-20 14:23:26 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,3 +1,13 @@
+
+def getsum(inputstring):
+   Total_sum = 0
+   current_digit = 0
+   
+
+
 def AddStringCalculator(inputstring):
+   if not inputstring:
+      return 0
+   return(getsum(inputstring))
    
   
