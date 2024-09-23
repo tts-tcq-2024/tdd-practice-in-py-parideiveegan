1286b07b2d10063804da674e3b2f66a25e468f2e
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-09-23 07:54:02 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -4,15 +4,16 @@ class TestStringCalculator(unittest.TestCase):
         
         def RetrunsZeroForEmptyStringInput(self):
                 self.assertEqual(AddStringCalculator(""), 0)                
-        #def ReturnsSumOfTwoNumbersSeparatedByCommaFormInputString(self):
-         #       self.assertEqual(AddStringCalculator("10,10"), 20)        
-        #def ReturnsSumOfMultipleNumbersSeparatedByCommaFromInputString(self):
-        #        self.assertEqual(AddStringCalculator("10,10,2,5"), 27)
-        #def ReturnsSumOfNumbersSeparatedByNewlineCommaFormInputString(self):
-        #        self.assertEqual(AddStringCalculator("10\n10,2\n1"), 23)
-        #def ReturnsSumOfNumbersSeparatedByAnyDelimitersUserSetsFormInputString(self):
-        #        self.assertEqual(AddStringCalculator("//?\n2?2,2\n2,2"), 10)
-        #def ThrowsExceptionMessageIfNegativeNumbersInTheInputString(self):             
+        def ReturnsSumOfTwoNumbersSeparatedByCommaFormInputString(self):
+                self.assertEqual(AddStringCalculator("10,10"), 20)        
+        def ReturnsSumOfMultipleNumbersSeparatedByCommaFromInputString(self):
+                self.assertEqual(AddStringCalculator("10,10,2,5"), 27)
+        def ReturnsSumOfNumbersSeparatedByNewlineCommaFormInputString(self):
+                self.assertEqual(AddStringCalculator("10\n10,2\n1"), 23)
+        def ReturnsSumOfNumbersSeparatedByAnyDelimitersUserSetsFormInputString(self):
+                self.assertEqual(AddStringCalculator("//?\n2?2,2\n2,2"), 10)
+        def ThrowsExceptionMessageIfNegativeNumbersInTheInputString(self):
+                self.assertEqual(AddStringCalculator(“-1,6,-22”),"negatives not allowed : -1,-22")
         def ReturnsSumOfNumbersButIgnoreNumbersGreaterThan1000InTheInputString(self):
                 self.assertEqual(AddStringCalculator("1,34534"), 1)
 
