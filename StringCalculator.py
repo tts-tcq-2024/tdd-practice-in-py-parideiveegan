dc50a194ade767481d68a5dbc0078306b33f7188
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-09-28 12:37:45 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,13 +1,43 @@
+def check_for_new_delimiter(inputstring, separators):
+    if inputstring[0]!='/':
+        return separators
+    delimiter = ''
+    index = 2
+    while inputstring[index] !='\n':
+        delimiter +=inputstring[index]
+        index +=1
+    separators.append(delimiter)
+    return [separators,index]
 
-def getsum(inputstring):
-   Total_sum = 0
-   current_digit = 0
-   
+def check_append(inputstring, index):
+    number_found = 0
+    delimiter = ''
+    while not number_found:
+        if inputstring[index]
 
+def get_numbers(inputstring, separators):
+    
+    refactored_inputstring = inputstring
+    delimiter = ''
+    number = ''
+    index = 0
+    result = []
+    numbers = []
+    while index <= len(inputstring)-1:
+        result = check_append(inputstring,index)
+        index = result[0]
+        number +=inputstring[index]
+        if result[1]:
+            numbers.append(number)
+            number = ''
 
 def AddStringCalculator(inputstring):
    if not inputstring:
       return 0
-   return(getsum(inputstring))
+   separators = ['\n',',']
+   changes = check_for_new_delimiter(inputstring,separators)
+   separators = changes[0]
+   inputstring = inputstring[(changes[1]+1):]
+
+   numbers = get_numbers(inputstring, separators)
    
-  
